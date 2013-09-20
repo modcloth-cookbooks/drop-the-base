c433923e2d067cb82be810f227a9d9c1ab69fbf6
Rafe Colton <r.colton@modcloth.com>
2013-09-20 17:53:03 -0400

    Initial setup && berks init

---
+++ README.md
@@ -0,0 +1,4 @@
+drop-the-base Cookbook
+======================
+
+Base role for all the things (including SmartOS, Ubuntu, and CentOS)
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'drop-the-base'
+maintainer       'ModCloth Inc.'
+maintainer_email 'platformsphere@modcloth.com'
+license          'MIT'
+description      'Installs/Configures drop-the-base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
