f9b303d48d4bc7128c926d71845f630160c82020
Rafe Colton <r.colton@modcloth.com>
2013-12-03 17:16:09 -0500

    Trying to make the ohai recipe not fail for ubuntu
    Signed-off-by: Jesse Szwedko <j.szwedko@modcloth.com>

--- recipes/ohai_plugins.rb
+++ recipes/ohai_plugins.rb
@@ -8,18 +8,21 @@
 
 include_recipe 'ohai'
 
-template "#{node['ohai']['plugin_path']}/cpu.rb" do
-  source 'plugins/cpu.rb.erb'
-  owner 'root'
-  group 'root'
-  mode 0755
-  only_if { node['platform'] == 'smartos' }
-end
+# Even though there could be guard statements, an if statement is required.
+# Without it, chef-client tries to look for the templates for Ubuntu and fails,
+# even though they will not actually get used.
+if node['platform'] == 'smartos'
+  template "#{node['ohai']['plugin_path']}/cpu.rb" do
+    source 'plugins/cpu.rb.erb'
+    owner 'root'
+    group 'root'
+    mode 0755
+  end
 
-template "#{node['ohai']['plugin_path']}/memory.rb" do
-  source 'plugins/memory.rb.erb'
-  owner 'root'
-  group 'root'
-  mode 0755
-  only_if { node['platform'] == 'smartos' }
+  template "#{node['ohai']['plugin_path']}/memory.rb" do
+    source 'plugins/memory.rb.erb'
+    owner 'root'
+    group 'root'
+    mode 0755
+  end
 end
