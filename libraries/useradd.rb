d674aa4b70b03eaf0eeaecb3e6889960b654c77f
Rafe Colton <r.colton@modcloth.com>
2013-12-09 18:30:52 -0800

    Correctly overriding the useradd provider if smartos

--- libraries/useradd.rb
+++ libraries/useradd.rb
@@ -2,110 +2,90 @@ class Chef
   class Provider
     class User
       class Useradd < Chef::Provider::User
+        if Chef::Node.load(Chef::Config['node_name']).platform?('smartos')
+          UNIVERSAL_OPTIONS = [[:comment, '-c'], [:gid, '-g'], [:password, '-p'], [:shell, '-s'], [:uid, '-u']]
 
-        def create_user
-          command = compile_command('useradd') do |useradd|
-            useradd << universal_options
-            useradd << useradd_options
-          end
-          run_command(command: command)
-          action_unlock
-        end
+          # May want to change default group from 'other' to 'username' to make smartos behavior similar to linux default.
+          # this would involve testing for gid and if not found creating group with username instead of defaulting to 'other'
 
-        def compare_user
-          changed = []
-          changed << [:comment, :home, :shell, :password].keep_if do |user_attrib|
-            !@new_resource.send(user_attrib).nil? && @new_resource.send(user_attrib) != @current_resource.send(user_attrib)
-          end
+          def create_user
+            command = compile_command('useradd') do |useradd|
+              useradd.concat(universal_options)
+              useradd.concat(useradd_options)
+            end.join(' ')
+            shell_out!(command)
 
-          changed << [:uid, :gid].keep_if do |user_attrib|
-            !@new_resource.send(user_attrib).nil? && @new_resource.send(user_attrib).to_i != @current_resource.send(user_attrib).to_i
+            # SmartOS locks new users by default until password is set
+            # unlock the account by default because password is set by chef
+            unlock_user if check_lock
           end
 
-          changed.flatten!.any?
-        end
-
-        def check_lock
-          case node['platform']
-          when 'smartos'
-            status = popen4("passwd -s #{@new_resource.username}") do |pid, stdin, stdout, stderr|
-              status_line = stdout.gets.split(' ')
-              case status_line[1]
-              when /^P/
-                @locked = false
-              when /^N/
-                @locked = false
-              when /^L/
-                @locked = true
-              end
+          def check_lock
+            passwd_s = shell_out!('passwd', '-s', new_resource.username, returns: [0,1])
+            status_line = passwd_s.stdout.split(' ')
+            case status_line[1]
+            when /^P/
+              @locked = false
+            when /^N/
+              @locked = false
+            when /^L/
+              @locked = true
             end
-          else
-            status = popen4("passwd -S #{@new_resource.username}") do |pid, stdin, stdout, stderr|
-              status_line = stdout.gets.split(' ')
-              case status_line[1]
-              when /^P/
-                @locked = false
-              when /^N/
-                @locked = false
-              when /^L/
-                @locked = true
-              end
+
+            unless passwd_s.exitstatus == 0
+              raise Chef::Exceptions::User, "Cannot determine if #{new_resource} is locked!"
             end
+
+            @locked
           end
 
-          unless status.exitstatus == 0
-            raise_lock_error = false
-            # we can get an exit code of 1 even when it's successful on rhel/centos (redhat bug 578534)
-            if status.exitstatus == 1 && platform_family?('rehl')
-              popen4('rpm -q passwd') do |pid, stdin, stdout, stderr|
-                passwd_version = stdout.gets.chomp
+          def lock_user
+            shell_out!('passwd', '-l', @new_resource.username)
+          end
 
-                unless passwd_version == 'passwd-0.73-1'
-                  raise_lock_error = true
+          def unlock_user
+            shell_out!('passwd', '-u', @new_resource.username)
+          end
+
+          def universal_options
+            opts = []
+
+            UNIVERSAL_OPTIONS.each do |field, option|
+              if @current_resource.send(field) != @new_resource.send(field)
+                if @new_resource.send(field)
+                  Chef::Log.debug("#{@new_resource} setting #{field} to #{@new_resource.send(field)}")
+                  opts << " #{option} '#{@new_resource.send(field)}'"
                 end
               end
-            else
-              raise_lock_error = true
             end
-
-            raise Chef::Exceptions::User, "Cannot determine if #{@new_resource} is locked!" if raise_lock_error
-          end
-
-          @locked
-        end
-
-        def lock_user
-          return if check_lock
-          case node['platform']
-          when 'smartos'
-            run_command(command: "passwd -l #{@new_resource.username}")
-          else
-            run_command(command: "usermod -L #{@new_resource.username}")
-          end
-        end
-
-        def unlock_user
-          return unless check_lock
-          case node['platform']
-          when 'smartos'
-            run_command(command: "passwd -u #{@new_resource.username}")
-          else
-            run_command(command: "usermod -U #{@new_resource.username}")
-          end
-        end
-
-        def useradd_options
-          case node['platform']
-          when 'smartos'
-            opts = ''
-            opts << ' -m' if updating_home? && managing_home_dir?
-            # opts << " -r" if @new_resource.system
+            if updating_home?
+              if managing_home_dir?
+                Chef::Log.debug("#{@new_resource} managing the users home directory")
+                opts << "-m -d '#{@new_resource.home}'"
+              else
+                Chef::Log.debug("#{@new_resource} setting home to #{@new_resource.home}")
+                opts << "-d '#{@new_resource.home}'"
+              end
+            end
+            opts << '-o' if @new_resource.non_unique || @new_resource.supports[:non_unique]
             opts
-          else
-            opts = ''
-            opts << ' -m' if updating_home? && managing_home_dir?
-            opts << ' -r' if @new_resource.system
-            opts
+          end
+
+          def useradd_options
+            []
+          end
+
+          def updating_home?
+            # will return false if paths are equivalent
+            # Pathname#cleanpath does a better job than ::File::expand_path (on both unix and windows)
+            # ::File.expand_path("///tmp") == ::File.expand_path("/tmp") => false
+            # ::File.expand_path("\\tmp") => "C:/tmp"
+            return true if @current_resource.home.nil? && @new_resource.home
+            @new_resource.home and Pathname.new(@current_resource.home).cleanpath != Pathname.new(@new_resource.home).cleanpath
+          end
+
+          def managing_home_dir?
+            @new_resource.manage_home || @new_resource.supports[:manage_home]
           end
         end
       end
